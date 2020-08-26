9a85f9ed87c9a9934b77af5819c7cd863257e71a
dchakro <35454738+dchakro@users.noreply.github.com>
2020-08-26 09:48:46 +0300

    Reading TSV benchmark

--- Benchmark.R
+++ Benchmark.R
@@ -1,42 +1,33 @@
 rm(list=ls())
 setwd("~/OneDrive - O365 Turun yliopisto/ExtraWorkSync/Klaus-Lab-Data/Big Data/BenchmarkR/COSMIC_test/subset/")
-# library(microbenchmark)
-# library(bench)
+library(microbenchmark)
+source("https://raw.githubusercontent.com/dchakro/shared_Rscripts/master/summarySE.R")
 
-
-
-
-
-
-bmark <- microbenchmark(
-  "base" = {
-    dat <- read.table(file = "test.tsv",header = T,sep = "\t",as.is = T,stringsAsFactors = T)
-    rm(dat);gc()
-  },
-  "readr" = {
-    dat <- readr::read_delim(file = "test.tsv",delim = "\t",progress=F)
-    rm(dat);gc()
-  },
-  "vroom" = {
-    dat <- vroom::vroom(file = "test.tsv",delim = "\t",progress = F)
-    rm(dat);gc()
-  }
-,times = 10)
-
-bmark
-
-
-bmark <- bench::mark(
-  "base" = {
-  dat <- read.table(file = "test.tsv",header = T,sep = "\t",as.is = T,stringsAsFactors = T)
-  rm(dat);gc()
-},
-"readr" = {
-  dat <- readr::read_delim(file = "test.tsv",delim = "\t",progress=F)
-  rm(dat);gc()
-},
-"vroom" = {
-  dat <- vroom::vroom(file = "test.tsv",delim = "\t",progress = F)
-  rm(dat);gc()
-})
+file.prefix <- c("11", "101", "1001", "10001", "100001", "1000001")
+Number.of.alternatives <- 3 # base, readr, vroom
+DF <- data.frame(expr="",N=NA,time=NA,sd=NA,se=NA,ci=NA,size=NA,stringsAsFactors = F)
+DF <- DF[-1,]
+for(f in file.prefix[6]){
+  file.name <- paste0(f,"_d.tsv")
+  bmark <- microbenchmark(
+    "base" = {
+      dat <- read.table(file = file.name,header = T,sep = "\t",as.is = T,stringsAsFactors = T)
+      rm(dat)
+    },
+    "readr" = {
+      dat <- readr::read_delim(file = file.name,delim = "\t",progress=F)
+      rm(dat)
+    },
+    "vroom" = {
+      dat <- vroom::vroom(file = file.name,delim = "\t",progress = F)
+      rm(dat)
+    }
+    ,times = 5)
+    gc()
+    saveRDS(bmark,file = paste0("bmark_reading_",f,".RDS"))
+    results <- summarySE(bmark,measurevar = "time",groupvars = "expr",statistic = "mean")
+    results$size <- rep(f,length(results[,1]))
+    DF <- rbind.data.frame(DF,results)
+    rm(results,bmark)
+}
 
