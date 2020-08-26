82b7207d235bd1f114433fead8bd49073ca64601
dchakro <35454738+dchakro@users.noreply.github.com>
2020-08-26 10:18:09 +0300

    reading and writing comparison

--- Benchmark.R
+++ Benchmark.R
@@ -3,15 +3,17 @@ setwd("~/OneDrive - O365 Turun yliopisto/ExtraWorkSync/Klaus-Lab-Data/Big Data/B
 library(microbenchmark)
 source("https://raw.githubusercontent.com/dchakro/shared_Rscripts/master/summarySE.R")
 
+## Reading TSV
+# base, readr, vroom
+test.name <- "readingTSV"
 file.prefix <- c("11", "101", "1001", "10001", "100001", "1000001")
-Number.of.alternatives <- 3 # base, readr, vroom
 DF <- data.frame(expr="",N=NA,time=NA,sd=NA,se=NA,ci=NA,size=NA,stringsAsFactors = F)
 DF <- DF[-1,]
 for(f in file.prefix){
   file.name <- paste0(f,"_d.tsv")
   bmark <- microbenchmark(
     "base" = {
-      dat <- base::read.table(file = file.name,header = T,sep = "\t",as.is = T,stringsAsFactors = T)
+      dat <- utils::read.table(file = file.name,header = T,sep = "\t",as.is = T,stringsAsFactors = T)
       rm(dat)
     },
     "readr" = {
@@ -24,10 +26,55 @@ for(f in file.prefix){
     }
     ,times = 5)
     gc()
-    saveRDS(bmark,file = paste0("../bmark/bmark_reading_",f,".RDS"))
+    saveRDS(bmark,file = paste0("../bmark/bmark_",test.name,"_",f,".RDS"))
     results <- summarySE(bmark,measurevar = "time",groupvars = "expr",statistic = "mean")
     results$size <- rep(f,length(results[,1]))
     DF <- rbind.data.frame(DF,results)
     rm(results,bmark)
 }
-saveRDS(DF,file = paste0("../results/results_reading_",f,".RDS"))
+saveRDS(DF,file = paste0("../results/results_",test.name,"_",f,".RDS"))
+rm(list=ls())
+gc()
+
+# Writing TSV
+# base, readr, vroom then saveRDS
+test.name <- "saveRDS"
+source("https://raw.githubusercontent.com/dchakro/shared_Rscripts/master/summarySE.R")
+file.prefix <- c("11", "101", "1001", "10001", "100001", "1000001")
+DF <- data.frame(expr="",N=NA,time=NA,sd=NA,se=NA,ci=NA,size=NA,stringsAsFactors = F)
+DF <- DF[-1,]
+for(f in file.prefix){
+  file.name <- paste0(f,"_d.tsv")
+  
+  dat <- readr::read_delim(file = file.name,delim = "\t",progress=F)
+  bmark_readr <- microbenchmark( "readr" = {
+      saveRDS(dat,file = "../tmp.dat.RDS")
+    },times = 5)
+  rm(dat)
+  
+  dat <- utils::read.table(file = file.name,header = T,sep = "\t",as.is = T,stringsAsFactors = T)
+  bmark_base <- microbenchmark( "base" = {
+    saveRDS(dat,file = "../tmp.dat.RDS")
+  },times = 5)
+  rm(dat)
+  
+  dat <- vroom::vroom(file = file.name,delim = "\t",progress = F)
+  bmark_vroom <- microbenchmark( "vroom" = {
+      saveRDS(dat,file = "../tmp.dat.RDS")
+    },times = 5)
+  rm(dat)
+  
+  bmark <- rbind.data.frame(bmark_base,bmark_readr,bmark_vroom) 
+  rm(bmark_base,bmark_readr,bmark_vroom)
+  gc()
+  saveRDS(bmark,file = paste0("../bmark/bmark_",test.name,"_",f,".RDS"))
+  results <- summarySE(bmark,measurevar = "time",groupvars = "expr",statistic = "mean")
+  results$size <- rep(f,length(results[,1]))
+  DF <- rbind.data.frame(DF,results)
+  rm(results,bmark)
+}
+saveRDS(DF,file = paste0("../results/results_",test.name,"_",f,".RDS"))
+rm(list=ls())
+gc()
+
+DF <- readRDS("../results/results_reading_1000001.RDS")
